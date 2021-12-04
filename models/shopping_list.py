6d437b0791c84bc0b3137e1a5ad7d0d1234cf308
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-04 17:08:13 +0000

    Minor display changes

--- models/shopping_list.py
+++ models/shopping_list.py
@@ -2,6 +2,6 @@ from models.item import Item
 
 item_1 = Item('Bran Flakes', 2.5, 2, False)
 item_2 = Item('Chocolate Orange', 1.0, 1, False)
-item_3 = Item('Washing-up Liquid', 1.99, 1.0, True)
+item_3 = Item('Washing-up Liquid', 1.99, 1, True)
 
 shopping_list = [item_1, item_2, item_3]
