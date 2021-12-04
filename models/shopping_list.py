b8f95f35146634ad0f5a9d5e06aa895791e88c64
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-04 16:15:41 +0000

    Created shopping_list and filled with items

---
+++ models/shopping_list.py
@@ -0,0 +1,7 @@
+from Item import Item
+
+item_1 = Item('Bran Flakes', 2.5, 2, False)
+item_2 = Item('Chocolate Orange', 1.0, 1, False)
+item_3 = Item('Washing-up Liquid', 1.99, 1.0, True)
+
+shopping_list = [item_1, item_2, item_3]
